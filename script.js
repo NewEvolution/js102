ee4daffdf8f1a1fbc6ca1075a3013ef931b5d740
Ryan Tanay <ryantanay@newevolution.org>
2015-07-28 09:12:54 -0500

    Completed step 5.

---
+++ script.js
@@ -0,0 +1,9 @@
+function greaterQuarter(numArr) {
+  var greaterArr = [];
+  for(var i=0; i<numArr.length; i++) {
+    if(numArr[i] > 25) {
+      greaterArr[greaterArr.length] = numArr[i];
+    }
+  }
+  return greaterArr;
+};
