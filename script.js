1f9e08ec6c174a31d1a4742758b34bff62501040
Ryan Tanay <ryantanay@newevolution.org>
2015-07-28 09:28:07 -0500

    I think I had 8 & 9 as part of the last commit, but here's 10-13 as well.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+function arrSorter(numArr) {
+  var sortedArr = numArr.sort();
+  return sortedArr;
+};
+
 function greaterQuarter(numArr) {
   var greaterArr = [];
   for(var i=0; i<numArr.length; i++) {
@@ -8,13 +13,15 @@ function greaterQuarter(numArr) {
   return greaterArr;
 };
 
-function arrSorter(numArr) {
-  var sortedArr = numArr.sort();
-  return sortedArr;
+function renderOutput(numArr) {
+  var outputString = "";
+  for(var i=0; i<numArr.length; i++) {
+    outputString += "<div class='number'>" + numArr[i] + "</div>";
+  }
 };
 
 $("#sort").click(function(e) {
   e.preventDefault;
   var userInput = $("#csv").value.split(",");
-  
+  renderOutput(greaterQuarter(arrSorter(userInput)))
 });
